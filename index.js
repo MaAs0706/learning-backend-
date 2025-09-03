775778dc07483eb91cfcfd5681473c63b3378e42
MaAs0706e <aswanthmadhav07@gmail.com>
2025-09-03 21:30:02 +0530

    new get

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get("/twiter",(req,res)=>{
+    res.send("my twitter handle ")
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
